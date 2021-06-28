4ce71fd5f79a4a7a087e84413a808d8857d4f4c6
Alvin-Karanja <alvin2@protonmail.com>
2021-06-28 09:10:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/countdowntimer/activity/Main2Activity.java
@@ -0,0 +1,70 @@
+package com.example.countdowntimer.activity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.countdowntimer.R;
+import com.example.countdowntimer.SimpleCountDownTimer;
+
+public class Main2Activity extends AppCompatActivity implements SimpleCountDownTimer.OnCountDownListener {
+    private TextView textView;
+    private Button start;
+    private Button resume;
+
+    private final SimpleCountDownTimer simpleCountDownTimer = new SimpleCountDownTimer(0, 10, 1, this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv);
+        start = findViewById(R.id.startBtn);
+        resume = findViewById(R.id.resumeBtn);
+        Button pause = findViewById(R.id.pauseBtn);
+
+        resume.setEnabled(false);
+
+        start.setOnClickListener(view -> {
+            simpleCountDownTimer.start(false);
+
+            start.setEnabled(false);
+
+        });
+
+        resume.setOnClickListener(view -> {
+            simpleCountDownTimer.start(true);
+            simpleCountDownTimer.runOnBackgroundThread();
+        });
+        pause.setOnClickListener(view -> {
+            simpleCountDownTimer.pause();
+            simpleCountDownTimer.setTimerPattern("s");
+            resume.setEnabled(true);
+        });
+
+
+    }
+
+    @Override
+    public void onCountDownActive(String time) {
+
+        textView.post(() -> textView.setText(time));
+
+        Toast.makeText(this, "Seconds = " + simpleCountDownTimer.getSecondsTillCountDown() + " Minutes=" + simpleCountDownTimer.getMinutesTillCountDown(), Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onCountDownFinished() {
+        textView.post(() -> {
+            textView.setText("Finished");
+            start.setEnabled(true);
+            resume.setEnabled(false);
+        });
+
+
+    }
+}
